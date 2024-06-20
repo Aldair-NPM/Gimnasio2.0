d2252d6fa9497303b8a9f3f4c737b300cd5160b4
Aldair <amadoraldair794@gmail.com>
2024-06-20 11:44:00 -0600

    insersion de usuarios en un array

--- BackEnd/routes/user.py
+++ BackEnd/routes/user.py
@@ -1,6 +1,30 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime 
 
 user = APIRouter()
-@user.get("/user")
-def hellowwordid():
-    return "Hola 9b"
+users = []
+
+#userModels
+class model_users(BaseModel):
+    id:str
+    usurio:str
+    contrasena:str
+    created_at:datetime = datetime.now()
+    estatus:bool=False
+
+@user.get("/")
+
+def bienvenida():
+    return "Bienvenido al sistema de apis"
+
+@user.get("/users")
+     
+def get_usuarios():
+    return users
+
+@user.post("/users")
+
+def save_usuarios(insert_users:model_users):
+    users.append(insert_users)
+    return "Datos guardados"
