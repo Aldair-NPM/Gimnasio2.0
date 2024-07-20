cd934d47c8cf85107d9f35d63af1244fe26b05f3
Aldair Amador Ibarra <amadoraldair794@gmail.com>
2024-07-19 18:13:30 -0600

    Creacion de usuario

--- Back-end/config/db.py
+++ Back-end/config/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://avnadmin:AVNS_DDFyfHbQ3t8OkXRLYc_@mysql-23d7c3b-amadoraldair794-5b4b.l.aivencloud.com:21641/defaultdb'
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Sagitario96@localhost:3306/test'  #Conexión local
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://avnadmin:AVNS_DDFyfHbQ3t8OkXRLYc_@mysql-23d7c3b-amadoraldair794-5b4b.l.aivencloud.com:21641/defaultdb'
+#SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Sagitario96@localhost:3306/test'  #Conexión local
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
